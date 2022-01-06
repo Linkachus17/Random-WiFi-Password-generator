2cc333e4d14029f219a000940fffa6c5a584b671
Linkachus17 <rg16111982@gmail.com>
2022-01-06 22:07:20 +0700

    UPDATED

--- pwdgen.py
+++ pwdgen.py
@@ -2,13 +2,25 @@ import string
 import random
 from time import sleep
 
+
+
+print("______ _    _______ _____  _____ _   _ \n"
+"| ___ \ |  | |  _  \  __ \|  ___| \ | |\n"
+"| |_/ / |  | | | | | |  \/| |__ |  \| |\n"
+"|  __/| |/\| | | | | | __ |  __|| . ` |\n"
+"| |   \  /\  / |/ /| |_\ \| |___| |\  |\n"
+"\_|    \/  \/|___/  \____/\____/\_| \_/\n")
+                                       
+                                       
+
+
 print("Welcome to PWDGEN\n")
 sleep(3)
-print("This program gives you strong password for your accounts\nBy using uppercase, lowercase, digits and punctuation in "
+print("This program gives you strong password for your accounts\nBy using uppercase, lowercase, digits, punctuation, hexdigits and octdigits in "
     "the password")
 sleep(3)
 print("========================================================\n")
-print("Use '914615' as input to show the info")
+print("Use '914615' as input to show the info\nUse CTRL + C to terminate")
 sleep(1)
 
 
@@ -22,21 +34,18 @@ def info():
 
 while True:
 
-
     usr_input = int(input("Enter the length of password (No limit): "))
 
     def randomString(stringLength=usr_input):
-        letters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+        letters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation + string.hexdigits + string.octdigits
         return ''.join(random.choice(letters) for i in range(stringLength))
     
-    if usr_input ==  914615:
+    if usr_input == 914615:
         info()
         sleep(10)
         print(randomString())
         sleep(1)
         print("\n\n")
-
-
 
     print(randomString())
     sleep(1)
